6d057563316622b22d2579f0ee003cae08fb9052
Marcela Porto <email": null>
2017-04-09 14:42:00 -0700

    adds user preferance page - weather input not working completely yet

--- app/jobs/alarm_queue.rb
+++ app/jobs/alarm_queue.rb
@@ -2,11 +2,11 @@ class AlarmQueue < ActiveJob::Base
   queue_as :default
 
   def perform(user_id)
+    puts "ALARRRRMMMM"
     user = User.find(user_id)
-    if Time.now == user.time.to_time 
+    if Time.now == user.time.to_time
       # method to construct the alarm event
-      puts "ALARRRRMMMM"
-    end 
+    end
   end
 
 end
