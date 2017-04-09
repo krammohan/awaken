e8e4d330a7910df6c3a7824124cd61302d7382ae
radiohegel <roger.e.clayton@gmail.com>
2017-04-09 16:29:37 -0700

    starts implementing jquery work around for widget toggles

--- app/assets/javascripts/datetime.js
+++ app/assets/javascripts/datetime.js
@@ -4,6 +4,12 @@ $(document).ready(function() {
     selectYears: 15 // Creates a dropdown of 15 years to control year
   })
 
+  $("#switch-is-on").on("click", function(){
+    var url = $(this).data("data")
+    console.log($(this))
+    console.log(url)
+  })
+
   function value_() {
   var checkbox = document.getElementById('mycheckbox');
   alert('checkbox value: ' + checkbox.checked);
