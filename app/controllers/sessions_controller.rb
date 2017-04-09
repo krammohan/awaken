0b1d3f2636a8834a918a877a842b425686f59686
krammohan <kavya.ramm@gmail.com>
2017-04-08 17:37:04 -0700

    oauth working

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,18 @@
 class SessionsController < ApplicationController
+  
   def new
+  	@user = current_user
   end
 
   def create
+	user = User.find_for_google_oauth2(env["omniauth.auth"])
+	session[:user_id] = user.id
+	redirect_to "home/index"
   end
 
   def destroy
+	session[:user_id] = nil
+	redirect_to root_path
   end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -21,8 +21,10 @@ ActiveRecord::Schema.define(version: 20170408192026) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "first_name"
-    t.string   "last_name"
+    t.string   "name"
+    t.string   "uid"
+    t.string   "username"
+    t.string   "provider"
     t.string   "access_token"
     t.string   "refresh_token"
     t.datetime "created_at",                          null: false
