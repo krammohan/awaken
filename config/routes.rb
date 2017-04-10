5a11aabc88f3596e82de4bac9f732c7e573e9d77
krammohan <kavya.ramm@gmail.com>
2017-04-10 10:41:29 -0700

    Changes location and route of omniauth controller

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
 
   root 'sessions#new'
 
-  devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks" }
+  devise_for :users, controllers: { omniauth_callbacks: "omniauth_callbacks" }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :sessions
