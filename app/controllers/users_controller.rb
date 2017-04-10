f2e0b2109e9101496e812126c594feea8e515374
“angelamarshall” <hello@angelasmarshall.com>
2017-04-09 21:59:58 -0700

    fixes the maps widget in user preferences

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -28,7 +28,7 @@ class UsersController < ApplicationController
 private
 
   def user_params
-    params.require(:user).permit(:time, :weather, :zip)
+    params.require(:user).permit(:time, :weather, :zip, :origin_location, :destination_location, :mode, :transit_mode, :maps)
   end
 
 end
