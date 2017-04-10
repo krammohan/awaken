49efb6484ed7774375db3b446faaf39ac48f3ba7
Marcela Porto <marcela.fporto@gmail.com>
2017-04-10 15:24:45 -0700

    Changes to turn off tv - tests

--- app/controllers/widgets_controller.rb
+++ app/controllers/widgets_controller.rb
@@ -3,7 +3,7 @@ class WidgetsController < ApplicationController
 	def index
 		@skip_layout = true
 		#### USED FOR TESTS ####
-		$pubnub.publish( channel: 'my_channel', message: { action: true, url: 'heroku' }) do |envelope|
+		$pubnub.publish( channel: 'my_channel', message: { action: false, url: 'heroku' }) do |envelope|
 				puts envelope.status
 			end
 		##### DELETE IN THE FUTURE ###
