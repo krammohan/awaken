d05f05803744297d0a5fcfa2cd1d0f27ec66b8a3
krammohan <kavya.ramm@gmail.com>
2017-04-09 19:37:30 -0700

    Adds ajax Google Matrix API call to get work transit info, adds location columns to user model

---
+++ db/migrate/20170410023123_add_location_to_users.rb
@@ -0,0 +1,8 @@
+class AddLocationToUsers < ActiveRecord::Migration[5.0]
+  def change
+  	add_column :users, :origin_location, :string
+  	add_column :users, :destination_location, :string
+  	add_column :users, :mode, :string
+  	add_column :users, :transit_mode, :string
+  end
+end
