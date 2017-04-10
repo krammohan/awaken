d85b01a6b9ad4f9399dd8b5c4f65ca404982e820
Marcela Porto <marcela.fporto@gmail.com>
2017-04-10 16:07:44 -0700

    Tests

--- app/jobs/alarm_queue.rb
+++ app/jobs/alarm_queue.rb
@@ -1,22 +1,25 @@
 class AlarmQueue < ActiveJob::Base
   queue_as :default
   def perform(user_id)
-    user = User.find(user_id)
-    if !(weekend?)
-      if Time.now.strftime("%I:%M") == user.time.strftime("%I:%M")
-        user.check_and_construct
-        # Generate Pub/Sub connection and turn on TV
-        $pubnub.publish( channel: 'my_channel', message: { action: true, url: 'heroku' }) do |envelope|
-            puts envelope.status
-          end
-        puts "ALARRRRMMMM"
-      elsif (Time.now.strftime("%I:%M") == ((user.time.to_time) + 20*60).strftime("%I:%M"))# if its been more than 20 minutes since the alarm went off, turn off the TV.
-        $pubnub.publish( channel: 'my_channel', message: { action: false }) do |envelope|
-            puts envelope.status
-          end
-        puts "GOOOOODBYE"
-      end
+    $pubnub.publish( channel: 'my_channel', message: { action: false }) do |envelope|
+      puts envelope.status
     end
+    # user = User.find(user_id)
+    # if !(weekend?)
+    #   if Time.now.strftime("%I:%M") == user.time.strftime("%I:%M")
+    #     user.check_and_construct
+    #     # Generate Pub/Sub connection and turn on TV
+    #     $pubnub.publish( channel: 'my_channel', message: { action: true, url: 'heroku' }) do |envelope|
+    #         puts envelope.status
+    #       end
+    #     puts "ALARRRRMMMM"
+    #   elsif (Time.now.strftime("%I:%M") == ((user.time.to_time) + 20*60).strftime("%I:%M"))# if its been more than 20 minutes since the alarm went off, turn off the TV.
+    #     $pubnub.publish( channel: 'my_channel', message: { action: false }) do |envelope|
+    #         puts envelope.status
+    #       end
+    #     puts "GOOOOODBYE"
+    #   end
+    # end
   end
   private
     def weekend?
