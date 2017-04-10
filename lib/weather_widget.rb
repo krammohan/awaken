39a27da3db4842295789372c72db3a95fb7bf97a
radiohegel <roger.e.clayton@gmail.com>
2017-04-10 15:47:17 -0700

    adds format_weather method for weather module

--- lib/weather_widget.rb
+++ lib/weather_widget.rb
@@ -3,5 +3,10 @@ module WeatherWidget
     location_hash = ZipCodes.identify(zip)
     location = "#{location_hash[:city]}, #{location_hash[:state_code]}"
     response = Weather.lookup_by_location(location, Weather::Units::FAHRENHEIT)
+    format_weather(response)
+  end
+
+  def self.format_weather(data)
+    "<div class='widget-container'><div id='weather-widget' class= 'hidden' style='background-color:rgba(0, 0, 0, 0.6);'><h3 text-align='center' color='white'>Weather<img id='weather-icon' src='#{data.image.url}' height='60' width='60'></img></h3><p id='weather'>#{data.location.city} <br>#{data.condition.temp}° <br>#{data.condition.text} <br>Wind: #{data.wind.speed}mph <br></p></div></div>"
   end
 end
