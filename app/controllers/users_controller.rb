f198868072a4d57b2da02a9adf841ee97e32839d
Marcela Porto <marcela.fporto@gmail.com>
2017-04-10 20:59:54 -0700

    Refactors

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,39 +6,23 @@ class UsersController < ApplicationController
 
 	def update
 		update_status = current_user.update(user_params)
-		flash.notice = "ALARM UPDATED THO"
 
-		# find the jobs and kill from the user
-		# rufus = scheduler.at('2017-04-10 17:14', :user_id => 1) do print "KAVYAAAAA" end
-		# 	time = "this is the time"
+			# Not working 100% => needs to know if it's the same day!!
+			#
+			# $scheduler.jobs(:user_id => current_user.id).each do |job|
+			# 			p job
+			# 			p "*" * 30
+			# 		if job.scheduled_at.includes? user.time.split(" ")[0]
+			# 			job.unschedule
+			# 		end
+			# end
 
-		p "*" * 30
-		p "Time"
-		p current_user.time
-		p "*" * 30
-		p "*" * 30
-
-			# Not working 100% => needs to know if it's the same day!
-			p $scheduler.jobs(:user_id => current_user.id)
-
-			$scheduler.jobs(:user_id => current_user.id).each do |job|
-						p job
-						p "*" * 30
-					# if job.scheduled_at.includes? user.time.split(" ")[0]
-					# 	job.unschedule
-					# end
-			end
-
-
-			# Time in UTC!!!
 			user_job = $scheduler.at(date_time, :user_id => current_user.id) do
 				# @user.construct_widgets
 				pub_nub_job
 			end
 
 			p user_job
-
-		# .strftime("%Y-%m-%d %H:%M")
 
     respond_to do |format|
         # AlarmQueue.perform_now(current_user.id)
@@ -66,7 +50,7 @@ private
 		$pubnub.publish( channel: 'my_channel', message: { action: true, url: 'heroku' }) do |envelope|
 				puts envelope.status
 			end
-		sleep (10)
+		sleep (10) # CHANGE THIS TIME
 		$pubnub.publish( channel: 'my_channel', message: { action: false }) do |envelope|
 				puts envelope.status
 			end
@@ -78,17 +62,9 @@ private
 		zone = "Pacific Time (US & Canada)"
 		datetime = ActiveSupport::TimeZone[zone].parse(params[:date] + " " + usertime)
 
-		# datetime = Time.parse(params[:date] + " " + usertime)
-
 		Time.zone = "Greenwich"
-
 		datetime.in_time_zone.strftime("%Y-%m-%d %H:%M")
 
-
-		# Time.parse(params[:date] + current_user.time.strftime("%H:%M"))
-    # date_parse = Date.parse(date)
-    # date_format = date_parse.strftime('%Y-%m-%d')
-    # date_format + " " + current_user.time.strftime("'%Y-%m-%d' %H:%M")
 	end
 
 end
