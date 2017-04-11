bc6a43d2fba5d751d89faf3ff861ce1ae092288c
Marcela Porto <marcela.fporto@gmail.com>
2017-04-10 17:43:50 -0700

    Creates singleton and adds rufus logic

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,7 +19,7 @@ class UsersController < ApplicationController
 					end
 			end
 
-			user_job = $scheduler.at('2017-04-10 17:42', :user_id => current_user.id) do
+			user_job = $scheduler.at('2017-04-10 17:45', :user_id => current_user.id) do
 				@user.construct_widgets
 				pub_nub_job
 			end
