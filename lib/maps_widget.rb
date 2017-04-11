a564de3768ce5e8ab614e188426729924873c4e6
krammohan <kavya.ramm@gmail.com>
2017-04-10 18:39:17 -0700

    Adds module to get transit info from Google using origin and destination addresses

---
+++ lib/maps_widget.rb
@@ -0,0 +1,26 @@
+module MapsWidget
+
+  def self.get_transit_info(user_origin, user_destination)
+  	  origin = parse_address(user_origin)
+  	  destination = parse_address(user_destination)
+	  url = "https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=#{origin}&destinations=#{destination}&key=AIzaSyClQh63PR-vCZbOpiExvqT2ZSKQsXPkrAo"
+	  response = HTTParty.get(url)
+	  # format_transit_info(response.parsed_response)
+	  response.parsed_response
+  end
+
+
+  def self.parse_address(addr)
+  	addr_arr = addr.split(" ")
+  	string_addr = ""
+  	addr_arr.each do |element|
+  	  string_addr += element + "+"
+  	end
+  	string_addr
+  end
+
+  def self.format_transit_info(data)
+    "<div id='maps-widget' class= 'hidden' style='background-color:rgba(0, 0, 0, 0.6);'><h3 text-align='center' color='white'>Transit</h3><p id='maps'>#{data} <br>#{data.condition.temp}° <br>#{data.condition.text} <br>Wind: #{data.wind.speed}mph <br></p></div>"
+  end
+
+end
