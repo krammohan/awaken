22d8ae9b4aca8887015cc87a112fe1f8652eb501
Marcela Porto <marcela.fporto@gmail.com>
2017-04-11 11:19:29 -0700

    Adds url to the user 1 in the pubnub command

--- app/controllers/widgets_controller.rb
+++ app/controllers/widgets_controller.rb
@@ -2,11 +2,8 @@ class WidgetsController < ApplicationController
 
 	def index
 		@skip_layout = true
-<<<<<<< HEAD
-=======
 		@content = User.find(params[:user_id]).content.html_safe
 	end
->>>>>>> 0bf1d4e0abdde21df36f441e6c57c208a67827b3
 	end
 
 end
