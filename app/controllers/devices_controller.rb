87ae2f70d834b22636d75cef67aabc3fb9d85f41
radiohegel <roger.e.clayton@gmail.com>
2017-04-12 08:15:36 -0700

    controller currently able to talk to python script

--- app/controllers/devices_controller.rb
+++ app/controllers/devices_controller.rb
@@ -1,5 +1,18 @@
 class DevicesController < ApplicationController
   def connect
-    
+    @serial = params[:id]
+    if Device.find_by(serial: @serial)
+      #send the matching url and pubnub connection info
+      #potentially redirect to a success page, telling users they are good to turn off their screen
+    else
+      respond_to do |format|
+        format.json do
+          render json: {
+            message: "success"
+          }.to_json
+        end
+      end
+      #Send url for the instructions handler and placeholder pubnub info
+    end
   end
 end
