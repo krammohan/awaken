91c73652545fa55a21131daba5e90a9566fe60cc
radiohegel <roger.e.clayton@gmail.com>
2017-04-11 19:41:21 -0700

    adds route for device connect

--- config/routes.rb
+++ config/routes.rb
@@ -8,11 +8,12 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :sessions
-  
+
   resources :users do
     resources :widgets
   end
 
   put 'users/:id/weather' => 'users#change_weather', as: "users_weather"
   put 'users/:id/maps' => 'users#change_maps', as: "users_maps"
+  get 'devices/:id' => 'devices#connect'
 end
