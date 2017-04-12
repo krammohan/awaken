5ec28590d43a283c179958c330411dfee13a6d82
Tony <saric.tony@gmail.com>
2017-04-12 16:47:55 -0700

    adds side scrolling news bar on the bottom of app

--- db/migrate/20170411213911_add_news_boolean_to_users.rb
+++ db/migrate/20170411213911_add_news_boolean_to_users.rb
@@ -1,5 +1,5 @@
 class AddNewsBooleanToUsers < ActiveRecord::Migration[5.0]
   def change
-    add_column :users, :news, :boolean, default: :false
+    add_column :users, :news, :boolean, default: :true
   end
 end
