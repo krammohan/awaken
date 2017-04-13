d4add4ce96cf129ef3b0923bc8ca7955e477eed9
krammohan <kavya.ramm@gmail.com>
2017-04-13 10:48:03 -0700

    Actually fixes it

--- lib/weather_widget.rb
+++ lib/weather_widget.rb
@@ -2,7 +2,7 @@ module WeatherWidget
   def self.get_weather(zip)
     location_hash = ZipCodes.identify(zip)
     location = "#{location_hash[:city]}, #{location_hash[:state_code]}"
-    response = Weather.lookup_by_location('', Weather::Units::FAHRENHEIT)
+    response = Weather.lookup_by_location('location', Weather::Units::FAHRENHEIT)
     format_weather(response)
   end
 
